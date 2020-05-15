c3e5a5a5a0704445381b92cda959393c834ccc81
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-05-15 16:30:29 -0300

    [REM] l10n_ar_account_withholding: aeroo dependency

--- ingadhoc/odoo-argentina/l10n_ar_account_withholding/__manifest__.py
+++ ingadhoc/odoo-argentina/l10n_ar_account_withholding/__manifest__.py
@@ -26,7 +26,7 @@
         'security/ir.model.access.csv',
         'security/security.xml',
         'wizard/res_config_settings_views.xml',
-        'reports/certificado_de_retencion_report.xml',
+        # 'reports/certificado_de_retencion_report.xml',
         'views/account_payment_group_view.xml',
         'views/afip_tabla_ganancias_escala_view.xml',
         'views/afip_tabla_ganancias_alicuotasymontos_view.xml',
@@ -49,7 +49,7 @@
         # para no sumar dependencinas. Se deberia requerir porque el reporte
         # usa las lineas
         # 'l10n_ar_aeroo_base',
-        'report_aeroo',
+        # 'report_aeroo',
     ],
     'external_dependencies': {
         'python': ['pyafipws'],
